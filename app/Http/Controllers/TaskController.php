422bc41e7dccc61cff1c084a6a93a20c48a1cf44
muhraedi <muhraedi@gmail.com>
2023-09-30 18:17:14 +0800

    Develop moving task feature

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -126,4 +126,15 @@ class TaskController extends Controller
             'tasks' => $tasks,
         ]);
     }
+
+    public function move(int $id, Request $request)
+    {
+        $task = Task::findOrFail($id);
+
+        $task->update([
+            'status' => $request->status,
+        ]);
+
+        return redirect()->route('tasks.progress');
+    }
 }
